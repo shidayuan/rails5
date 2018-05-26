dd9c274081ecf59fbfb669d059246e82275fa473
shichengyuan <15036062755@163.com>
2018-05-26 16:46:59 +0800

    skdjf

--- yuan/rails5/config/routes.rb
+++ yuan/rails5/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :groups
+  resources :groups do
+    member do
+      post :join
+      post :quit
+    end
 end
+end 
