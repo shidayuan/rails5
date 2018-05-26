ec38285f2fc8851f9872e8aaece83e7d99f79155
shichengyuan <15036062755@163.com>
2018-05-26 15:57:59 +0800

    skdjfls

--- yuan/rails5/app/controllers/groups_controller.rb
+++ yuan/rails5/app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -29,11 +30,6 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "you have no permission"
-    end
 
     if @group.update(group_params)
     redirect_to groups_path,notice: "编辑成功"
@@ -43,12 +39,7 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
 
-    if current_user != @group.user
-      redirect_to root_path, alert: "you have no permission"
-    end
-    
     @group.destroy
     redirect_to groups_path,alert: "删除成功"
   end
@@ -56,6 +47,13 @@ class GroupsController < ApplicationController
 
 
   private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "you have no permission"
+    end
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
