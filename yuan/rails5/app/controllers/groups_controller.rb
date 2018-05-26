d82e6d7d044beebb99239ef894f728cbbc88db86
zhaozongchang <xingyingsuifeng@gmail.com>
2018-05-26 15:11:11 +0800

    implement group#show

--- yuan/rails5/app/controllers/groups_controller.rb
+++ yuan/rails5/app/controllers/groups_controller.rb
@@ -13,9 +13,24 @@ class GroupsController < ApplicationController
     redirect_to groups_path
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end 
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to groups_path
+  end
+
+
   private
 
   def group_params
     params.require(:group).permit(:title, :description)
-  end 
+  end
 end
