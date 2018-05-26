bab1cb4b8baa88b780319498e6d190e35c79a6aa
shichengyuan <15036062755@163.com>
2018-05-26 15:15:01 +0800

    install devise

--- yuan/rails5/db/schema.rb
+++ yuan/rails5/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180526065247) do
+ActiveRecord::Schema.define(version: 20180526071322) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
